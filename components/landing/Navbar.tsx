7db6675fb86867412cb4ea0b3ffa4852ddb2c0a8
Lalit Kumar <lalitchauhan1517@gmail.com>
2026-03-27 00:50:32 +0530

    fix(navbar): set initial scrolled state on mount

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -9,6 +9,8 @@ export default function Navbar() {
 
   useEffect(() => {
     const onScroll = () => setScrolled(window.scrollY > 16);
+    // set initial state on mount in case the page is already scrolled
+    onScroll();
     window.addEventListener("scroll", onScroll, { passive: true });
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
